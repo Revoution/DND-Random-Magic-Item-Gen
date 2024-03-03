c21cbb8ce63df7f51c39a462a8429bdd04c3584b
FeralHoneyBadger <Williamjorstad@gmail.com>
2024-03-03 10:51:04 -0500

    Create #DND Random encounter Gen.py

---
+++ #DND Random encounter Gen.py
@@ -0,0 +1,111 @@
+#DND Random Gen Magic Item Table B
+
+import random
+
+Number = (random.randint(1,100))
+
+if 1 <= Number <= 15: 
+    print("Potion of greater healing")
+
+elif 16 <= Number <= 22:
+    print("Potion of fire breath")
+
+elif 23 <= Number <= 29:
+    print("Potion of resistance")
+
+elif 30 <= Number <= 34:
+    print("Ammunition, +1")
+
+elif 35 <= Number <= 39:
+    print("Potion of animal friendship")
+
+elif 40 <= Number <= 44:
+    print("Potion of hill giant strength")
+
+elif 45 <= Number <= 49:
+    print("Potion of growth")
+
+elif 50 <= Number <= 54:
+    print("Potion of water breathing")
+
+elif 55 <= Number <= 59:
+    print("Spell scroll 2nd level")
+
+elif 60 <= Number <= 64:
+    print("Spell scroll 3rd level")
+
+elif 65 <= Number <= 67:
+    print("Bag of holding")
+
+elif 68 <= Number <= 70:
+    print("Keoghtom's ointment")
+
+elif 71 <= Number <= 73:
+    print("Oil of slipperness")
+
+elif 74 <= Number <= 75:
+    print("Dust of disappearance")
+
+elif 76 <= Number <= 77:
+    print("Dust of dryness")
+
+elif 78 <= Number <= 79:
+    print("Dust of sneezing and choking")
+
+elif 80 <= Number <= 81:
+    print("Elemental Gem")
+
+elif 82 <= Number <= 83:
+    print("Philter of love")
+
+elif Number == 84:
+    print("Alchemy jug")
+
+elif Number == 85:
+    print("Cap of water breathing")
+
+elif Number == 86:
+    print("Cloak of the manta ray")
+
+elif Number == 87:
+    print("Driftglobe") 
+
+elif Number == 88:
+    print("Goggles of the night")
+
+elif Number == 89:
+    print("Helm of comprehending languages")
+
+elif Number == 90:
+    print("Immovable rod")
+
+elif Number == 91:
+    print("Lantern of revealing")
+    
+elif Number == 92:
+    print("Mariner's Armor")
+
+elif Number == 93:
+    print("Mithral Armor") 
+
+elif Number == 94:
+    print("Potion of poison")
+
+elif Number == 95:
+    print("Ring of swimming")
+
+elif Number == 96:
+    print("Robe of useful items")
+
+elif Number == 97:
+    print("Rope of climbing")
+
+elif Number == 98:
+    print("Saddle of the cavalier")
+
+elif Number == 99:
+    print("Wand of magic detection")
+
+else:
+    Number == 100
+    print("Wand of secrets")
